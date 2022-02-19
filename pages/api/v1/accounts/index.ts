5819308ec3ae01fbd14619c85166ff91034a6a09
vamidi <valencio_masaki16@live.nl>
2022-02-19 13:14:48 +0100

    release 2020.1.7f2 - fix prod build script

--- pages/api/v1/accounts/index.ts
+++ pages/api/v1/accounts/index.ts
@@ -1,12 +1,10 @@
-import { Request, Response } from 'express';
+import { NextApiRequest, NextApiResponse } from 'next';
 import cors from '../../middlewares/cors';
 
-export default async (req: Request, res: Response) =>
+export default async (req: NextApiRequest, res: NextApiResponse) =>
 {
 	// Run cors
 	await cors(req, res);
-
-	console.log(req, res);
 
 	return res.status(200).json({ message : 'Ok'});
 }
